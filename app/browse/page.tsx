4b61dd27494602efcfa1668de2e2f51bdf4836dc
David <duwagbale07@gmail.com>
2025-03-05 22:57:04 +0100

    Fix (button): Extend the base props for full customizable functionality

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -5,14 +5,15 @@ import ScanImage from "../ui/image-scan";
 
 
 export default function Query(){
+    const prevRoute = "/browse/upload";
 
     return (
         <div className="w-full h-full flex flex-col gap-5">
             
             <div className="flex items-center justify-start">
-                <a className="flex items-center gap-2 hover:underline hover:underline-offset-4">
+                <a href={prevRoute} className="flex items-center gap-2 hover:underline hover:underline-offset-4">
                     <ArrowLeftIcon className="h-5 w-5" />
-                    Back
+                    <span>Back</span>
                 </a>
             </div>
 
